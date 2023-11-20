e337d58c1f2a129c1b351b7105e3be47a2f480df
jayy122 <jetim186@gmail.com>
2023-11-20 03:20:51 +0100

    initial commit

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -7,7 +7,7 @@ function HomeScreen() {
       <div className="flex flex-col w-30  bg-gray-500">Side Bar</div>
       <div className="flex flex-col w-full flex-1">
         <div className="h-10 bg-gray-500">Navigation Bar</div>
-        <div className="flex flex-col gap-10 w-full">
+        <div className="flex flex-col gap-10 w-full transition duration-300 ease">
           <Outlet />
         </div>
       </div>
